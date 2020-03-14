4566e452eab17ad38e98114670a129038faaf1be
andreearamona1092 <andreea_ramona_olteanu@yahoo.com>
2020-03-14 21:57:56 +0200

    Second commit - Adding class Bride and constructor

---
+++ src/ro/ase/cts/GitAssignment/Bride.java
@@ -0,0 +1,15 @@
+package ro.ase.cts.GitAssignment;
+
+public class Bride {
+	public String name;
+	public String weddingLocation;
+	public double budget;
+	
+	public Bride(String name, String weddingLocation, double budget) {
+		this.name = name;
+		this.weddingLocation = weddingLocation;
+		this.budget = budget;
+	}
+	
+
+}
